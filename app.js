075b242b66ed5e2d8b1a7b55666ef76e0ae08683
云信 <rongjun.wangrj@alibaba-inc.com>
2017-05-31 12:27:17 +0800

    财报

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var exchange_rate = require('./routes/exchange_rate');
+var financial_report = require('./routes/financial_report');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/exchange_rate', exchange_rate);
+app.use('/financial_report', exchange_rate);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

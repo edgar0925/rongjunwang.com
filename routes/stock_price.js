1be5d6f8eeba2b2df5441c9dfe64b74c94bc80a4
云信 <rongjun.wangrj@alibaba-inc.com>
2017-07-28 20:51:13 +0800

    url替换

--- routes/stock_price.js
+++ routes/stock_price.js
@@ -287,9 +287,9 @@ function postRobotMessage(content)
 
 	var postOptions = {
 		// 钱庄
-		// url: 'https://oapi.dingtalk.com/robot/send?access_token=c22f1cbdc4149025f26243e351e786574024a547136ff0eec0b7cb5fb57e066d',
+		url: 'https://oapi.dingtalk.com/robot/send?access_token=c22f1cbdc4149025f26243e351e786574024a547136ff0eec0b7cb5fb57e066d',
 		// 测试
-		url: 'https://oapi.dingtalk.com/robot/send?access_token=1ed9af8c0be743a2933c57e0392bcc31484d24cdb26b289b400399b9c4cc6ce8',
+		// url: 'https://oapi.dingtalk.com/robot/send?access_token=1ed9af8c0be743a2933c57e0392bcc31484d24cdb26b289b400399b9c4cc6ce8',
 		method: "POST",
 		json:true,
 		headers: {
